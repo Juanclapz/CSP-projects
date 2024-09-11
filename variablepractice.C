617c89752d4319fae2e07785e6d27e2db174b395
Juanclapz <juan.franco@ucas-edu.net>
2024-09-11 20:41:42 +0000

    variablepractice.C

---
+++ variablepractice.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+        char name[] = "Juan Franco";
+        char lownumber[] = "5";
+        char highnumber[] = "150";
+        char breakfast[] = "eggs";
+        char favoritecolor[] = "black";
+        char schoolname[] = "UCAS";
+        char year[] = "2024";
+        char eyecolor[] = "brown";
+        char age[] = "15";
+        char favoritesubject[] = "geography";
+        printf("%s\n", name);
+        printf("%s\n", lownumber);
+        printf("%s\n", highnumber);
+        printf("%s\n", breakfast);
+        printf("%s\n", favoritecolor);
+        printf("%s\n", schoolname);
+        printf("%s\n", year);
+        printf("%s\n", eyecolor);
+        printf("%s\n", age);
+        printf("%s\n", favoritesubject);
+    return 0;
+}
