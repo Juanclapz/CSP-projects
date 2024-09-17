0371a9f90d9123d1f893d224c1e2d0d1b89346f4
Juanclapz <juan.franco@ucas-edu.net>
2024-09-17 20:17:56 +0000

    firstprogramC

---
+++ Variables/firstprogram.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[30];
+    printf("please tell me your name: \n");
+    fgets(name, sizeof(name), stdin);
+    printf("hello %s", name);
+    return 0;
+}
