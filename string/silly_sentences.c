df67e2776d455388fef308be4a3e9664cd11634d
Juanclapz <juan.franco@ucas-edu.net>
2024-09-26 04:34:50 +0000

    name decorator py

--- string/silly_sentences.c
+++ string/silly_sentences.c
@@ -3,22 +3,22 @@
 
 int main(void){
     char name[20], place[20], verb[20], noun[20], sentence[500];
-    printf("type a name: ");
+    printf("Type a name: ");
     scanf("%s", name);
-    printf("type a place: ");
+    printf("Type a place: ");
     scanf("%s", place);
-    printf("type a verb: ");
+    printf("Type a past tense verb: ");
     scanf("%s", verb);
-    printf("type a noun: ");
+    printf("Type a noun: ");
     scanf("%s", noun);
     strcat(sentence, name);
     strcat(sentence, " went to the ");
-    strcat(sentence, place);
-    strcat(sentence, " where they ");
-    strcat(sentence, verb);
-    strcat(sentence, " and bought ");
+    strcat(sentence,place);
+    strcat(sentence," where they ");
+    strcat(sentence,verb);
+    strcat(sentence, " and bought a ");
     strcat(sentence, noun);
-    strcat(sentence, ".");
-    printf("%s went to the %s where they %s and bought a %s.", name, place, verb, noun);
+    strcat(sentence, ".\n");
+    printf("%s", sentence);
     return 0;
 }
