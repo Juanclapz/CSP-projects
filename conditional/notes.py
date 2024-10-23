1b86fbae6d108f751f87e4c0c79d9ca17e7a2c03
Juanclapz <juan.franco@ucas-edu.net>
2024-10-23 19:11:02 +0000

    old enough python

---
+++ conditional/notes.py
@@ -0,0 +1,12 @@
+age = 15
+voter = "yes"
+if age >= 18 and voter == "yes":
+    print("You are able to vote!")
+elif age >= 16:
+    print("You are old enough to drive!")
+elif age >= 15:
+    print("You are old enough to get a learners permit!")
+elif age >= 5:
+    print("You are old enough to go to school!")
+elif age >= 1:
+    print("You are too young for school!")
